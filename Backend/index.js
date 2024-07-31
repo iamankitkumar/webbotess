9bd2e2994ec0312f8a9e48bb36e6867c3a17b2d9
Rinshu Shukla <Rinshu1157@gmail.com>
2024-07-31 01:13:48 -0700

    api

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3001;
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+app.get("/Course-card", (req, res) => {
+  const response = {
+    title: "Course Title",
+    image:
+      "https://imgs.search.brave.com/cU4tCweiWzs3p52Fl1WOKTFhcv7gKA2t4urxuamaBws/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzA1LzAwLzgyLzMw/LzM2MF9GXzUwMDgy/MzA1Nl9QamdzY1ZX/Qk05d3hyNzdZYlR0/ZWdlS0ptYWlQb0Va/Zy5qcGc",
+    description: "This is a brief description of the course.",
+  };
+
+  res.json(response);
 });
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}/`);
+  console.log(`Server is running at http://localhost:${port}/`);
 });
