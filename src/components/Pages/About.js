1437b9454667f577f5892e51423b2cb4bedca465
dhananjay gupta <dhananjaygupta3333@gmail.com>
2024-08-17 15:07:45 +0530

    add

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -1,8 +1,33 @@
 import React from 'react'
-
+import './About.css'
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <div className="responsive-container-block bigContainer">
+        <div className="responsive-container-block Container">
+          <p className="text-blk heading">
+            About Us
+          </p>
+          <p className="text-blk subHeading">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+          </p>
+          <div className="social-icons-container">
+            <a className="social-icon" href='as'>
+              <img className="socialIcon image-block" src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/bb33.png" alt='' />
+            </a>
+            <a className="social-icon">
+              <img className="socialIcon image-block" src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/bb34.png" alt='' />
+            </a>
+            <a className="social-icon">
+              <img className="socialIcon image-block" src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/bb35.png" alt='' />
+            </a>
+            <a className="social-icon">
+              <img className="socialIcon image-block" src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/bb36.png" alt='' />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
